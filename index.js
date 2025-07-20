4541a1e12fab27e27f5ef1b802c920036fdd85b4
TobixTech <tobixtech@gmail.com>
2025-07-20 14:10:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 // --- Middleware ---
 app.use(cors({
